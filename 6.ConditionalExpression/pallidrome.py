499cb83e0222e7b33f27e7c77afb2e60008c65aa
siddh-jain01 <siddhjain430@gmail.com>
2026-05-22 23:56:45 +0530

    new code

---
+++ 6.ConditionalExpression/pallidrome.py
@@ -0,0 +1,11 @@
+n = int(input("Enter a number: "))
+temp = n
+reverse = 0
+
+while temp != 0:
+    reverse = reverse * 10 + (temp % 10)
+    temp //= 10
+if n == reverse:
+    print("Palindrome Number")
+else:
+    print("Not a Palindrome Number")
